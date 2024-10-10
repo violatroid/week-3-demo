4a70c0a6eeb926ce060129b0cd84c024c15554d5
Yun Johnson <violatroid@gmail.com>
2024-10-10 08:07:26 -0400

    updateee

---
+++ conditional.js
@@ -0,0 +1,13 @@
+
+// COMPUTER VALUES ARE 0 and 1, AKA OFF and ON 
+// LOGIC TRUE OR FALSE, 0 is usually false and non-zero is usually true
+
+// Bool - Boolean (True/False) Data Type - Reason/Philosophy into math?
+
+hasLicense = false
+
+console.log(hasLicense)
+Node.js
+if(hasLicense){ //act as a gate to prevent us from using it 
+    console.log("Zoom Zoom"); // did not run 
+}
---
+++ conditional2.js
@@ -0,0 +1,24 @@
+// LOGICAL OPERATORS
+// && AND (all must be true) 
+// || OR (either must be true)
+// == comparison (are the same)
+// === strict comparison (compares both value and data type)
+// ! = NOT Equal (not the same)
+// ! == strict
+// >= greater than or equal to
+// <= less than or equal to 
+// > greater than 
+// < less than 
+
+height = 60
+minimum = 55
+
+console.log ("START"); // unconditional - always
+
+if (height >= minimum){
+    console.log ("Wooooo!!!!!!!!");
+}else{
+    console.log ("watching the fun");
+}
+
+console.log("END"); // unconditional - always 
